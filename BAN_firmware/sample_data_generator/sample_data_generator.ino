c853cac0d74fb9a59a4fc813286bfa92973b948b
Faith Cyril <cyrilfaith17@gmail.com>
2024-02-22 02:31:44 +0000

    Added sample data generator script for testing dynamodb routing from IoTcore

--- BAN_firmware/sample_data_generator/sample_data_generator.ino
+++ BAN_firmware/sample_data_generator/sample_data_generator.ino
@@ -7,19 +7,13 @@ void setup() {
 
 
 void loop() {
-  // Sample data
-  float temperature = random(20, 40); // Random temperature between 20 and 40
-  int BPM = random(60, 100); // Random heart rate between 60 and 100
-  int SpO2 = random(90, 100); // Random SpO2 between 90 and 100
-  int systolicPressure = random(100, 140); // Random systolic pressure between 100 and 140
-  int diastolicPressure = random(60, 90); // Random diastolic pressure between 60 and 90
-  sendData(temperature, BPM, SpO2, systolicPressure, diastolicPressure); // Publish sample data
+  sendTestData(); // Publish sample data
   delay(2000); // Delay for 5 seconds before publishing next message
 }
 
 
 //data sending functions
-void sendData(){
+void sendTestData(){
 device = getClient();
 if (!device.connected())
 {
@@ -28,15 +22,16 @@ if (!device.connected())
 else
 {
   device.loop();
+  float temperature = random(20, 40); // Random temperature between 20 and 40
+  int BPM = random(60, 100); // Random heart rate between 60 and 100
+  int SpO2 = random(90, 100); // Random SpO2 between 90 and 100
+  int systolicPressure = random(100, 140); // Random systolic pressure between 100 and 140
+  int diastolicPressure = random(60, 90); // Random diastolic pressure between 60 and 90
   publishMessage(temperature,avgBPM,avgSpO2,systolicPressure,diastolicPressure);
-  Serial.print(temperature);
-  Serial.print(" ");
-  Serial.print(avgBPM);
-  Serial.print(" ");
-  Serial.print(avgSpO2);
-  Serial.print(" ");
-  Serial.print(systolicPressure);
-  Serial.print(" ");
-  Serial.println(diastolicPressure);
+  Serial.print("Temperature: "); Serial.println(temperature);
+  Serial.print("BPM: "); Serial.println(BPM);
+  Serial.print("SpO2: "); Serial.println(SpO2);
+  Serial.print("Systolic Pressure: "); Serial.println(systolicPressure);
+  Serial.print("Diastolic Pressure: "); Serial.println(diastolicPressure);
 }
 }
