867aa8944ac1d1f091fa51fce6225d19358fc1d9
Faith Cyril <cyrilfaith17@gmail.com>
2024-02-28 22:43:47 +0000

    Added Lmabda to timescaledb forwarder script and dockerfile

---
+++ Lambda_To_Timescale/lambda.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+conn_str = "postgresql://server:password@host:port/database"
+conn = psycopg2.connect(conn_str)
+cursor = conn.cursor()
+
+def handler(event,context):
+    try:
+        query = f"INSERT INTO DeviceData (timestamp, device_id, temperature, heart_rate, oxygen_saturation, systolic_pressure, diastolic_pressure)  VALUES (NOW(),'{event['device_id']}', {event['temperature']}, {event['heart_rate']}, {event['oxygen_saturation']}, {event['systolic_pressure']}, {event['diastolic_pressure']});"
+        cursor.execute(query)  # Execute the SQL query
+        conn.commit()
+        return f"{event} inserted successfully"
+    except Exception as e:
+        return e
+
+
+
+
