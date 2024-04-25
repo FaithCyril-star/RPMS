bdbcb20924722cf9f5272b8261da1e55571ad0fc
Faith Cyril <cyrilfaith17@gmail.com>
2024-04-25 16:46:44 +0000

    Added button to cahracter mapping comment

--- BAN_firmware/slave/slave.ino
+++ BAN_firmware/slave/slave.ino
@@ -3,15 +3,14 @@
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
 char key;
-long integer;
-int flag;
-
+long integer = 0;
+int flag = 0;
 
 char keys[ROWS][COLS] = {
-  {'1','A','3','2'},
-  {'4','B','6','5'},
-  {'7','C','9','8'},
-  {'*','D','#','0'}
+{'1', '4', '7', '*'},
+{'2', '5', '8', '0'},
+{'3', '6', '9', '#'},
+{'A', 'B', 'C', 'D'}
 };
 byte rowPins[ROWS] = {11, 10, 9, 8}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {6, 5, 4, 3}; //connect to the column pinouts of the keypad
@@ -19,7 +18,7 @@ byte colPins[COLS] = {6, 5, 4, 3}; //connect to the column pinouts of the keypad
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 void setup(){
   Serial.begin(115200); 
-  Wire.begin(8);  ////UNO-Slave Address
+  Wire.begin(8);  ////nano-Slave Address
   Wire.onRequest(sendEvent);
 }
 
@@ -43,7 +42,6 @@ void loop(){
     Serial.print("Decimal Number: ");
     Serial.println(integer);
     }
-
     flag = 1; //new data
   }
 }
@@ -56,3 +54,18 @@ void sendEvent()
   Wire.write(data);
   flag = 0; //data is old
 }
+
+/*
+S1 - 1
+S5 - 2
+S9 - 3
+S2 - 4
+S6 - 5
+S10 - 6
+S3 - 7
+S7 - 8
+S11 - 9
+S4 - move to next value
+S8 - 0
+S12 - Clear
+*/
